3246f6a1c94d13d0504aab733bd4ea54c800a489
Дмитрий Войцехович <133788382+Omage208@users.noreply.github.com>
2023-05-19 18:20:02 +0300

    Ping - Pong.py

--- ping.py
+++ ping.py
@@ -13,6 +13,12 @@ recket_x = 200
 recket_y = 330
 
 
+class Area():
+    def __init__(self, x = 0, y = 0, width = 10, height = 10):
+        self.rect = Rect(x, y, width, height)
+    def fill(self):
+        draw.rect(background, self.fill_color, self.rect)
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y,size_x, size_y,  player_speed):
         super().__init__()
@@ -30,13 +36,38 @@ class Player(GameSprite):
         if keys[K_RIGHT] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
 
+class Label(Area):
+    def set_text(self, text, fsize = 50, text_color = (0, 0, 0)):
+        self.image = font.SysFont('Arial', fsize).render(text, True, text_color)
+
+    def draw(self, shift_x = 0, shift_y = 0):
+        self.fill()
+        background.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+
+class Picture(Area):
+    def __init__(self, filename, x = 0, y = 0, width = 10, height = 10):
+        Area.__init__(self, x = x, y = y, width = width, height = height)
+        self.image = image.load(filename)
+    def draw (self):
+        background.blit(self.image, (self.rect.x, self.rect.y))
+    def colliderect(self, enemy):
+        return self.rect.colliderect(enemy)
+
 for i in range(3):
     ball = (img_ball, -40, 80, 50)
 
-ball = ('ball.jpg', 160, 200, 50, 50)
-platform1 = ('platform1.jpg', 160, 200, 50, 50)
-platform2 = ('platform2.jpg', 1950, 450, 50, 50)
+ball = Picture('ball.jpg', 160, 200, 50, 50)
+platform1 = Picture('platform1.jpg', 160, 200, 50, 50)
+platform2 = Picture('platform2.jpg', 1950, 450, 50, 50)
 
+font1 = font.Font(None, 35)
+lose1 = fon1.render('player 1 LOSE!', True, (180, 0, 0))
+
+font2= font.Font(None, 35)
+lose2 = font2.render('player 2 LOSE!', True, (180, 0, 0))
+
+move_down = False
+move_up = False
 finish = False
 speed_x = 3
 cpeed_y = 3
@@ -50,18 +81,36 @@ while not game:
         ball.rect.x += speed_x
         ball.rect.y += speed_y
 
-    if ball.rect.y > win_height - 50:
-        or ball.rect.y < 0:
+    if ball.rect.y > win_height - 50 or ball.rect.y < 0:
             speed_y *= -1
 
-    if sprite.collide_rect(platorm1, ball):
-        or sprite.collide_rect(platform2, ball):
+    if sprite.collide_rect(platorm1, ball) or sprite.collide_rect(platform2, ball):
             speed_x *= -1
 
     if ball.rect.x < 0:
         finish = True
 
+    if move_down:
+        platform.rect.y -= 3
+    
+    if move_down:
+        platform.rect.y += 3
+
+    if move_up:
+        platform.rect.y -= 3
+    
+    if move_up:
+        platform.rect.y += 3
+
     window.blit(background, (0 ,0))
+
+    ball.draw()
+    platform1.draw()
+    platform2.draw()
+
+    ball.update()
+    platform1.update()
+    platform2.update()
 
     display.update()
     clock.tick(50)
